88d92264434c61f62af8c98393bfd49bebc1813d
Dmitrii <dmasny@yandex.ru>
2023-03-19 20:14:35 +0300

    wip

--- factory.sol
+++ factory.sol
@@ -3,13 +3,12 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "./sbt.sol";
 
-
 contract Factory{
     mapping (string => address) private _plugs;
 
     function createPlug(string memory institution, string memory doc_name, address own) external {
-        ObjectCertificates soulbound = new ObjectCertificates(institution, doc_name, own);
-        _plugs[institution] = address(soulbound);
+        ObjectCertificates non_sbt = new ObjectCertificates(institution, doc_name, own);
+        _plugs[institution] = address(non_sbt);
     }
     
     function GetPlug(string memory authority_name) public view returns(address) {
